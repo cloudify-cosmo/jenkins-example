68e652b2c232ef72be2908d94267efa1b45bec57
Igor Zhivilo <igorzhivilo@Igors-MacBook-Pro.local>
2020-05-10 19:25:46 +0300

    UT success

--- test.py
+++ test.py
@@ -9,18 +9,18 @@ class TestHello(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_hello(self):
-        rv = self.app.get('/3')
+        rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_hello(self):
-        rv = self.app.get('/hello12/')
+        rv = self.app.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_name(self):
         name = 'Simon'
-        rv = self.app.get(f'/hello123/{name}')
+        rv = self.app.get(f'/hello/{name}')
         self.assertEqual(rv.status, '200 OK')
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
