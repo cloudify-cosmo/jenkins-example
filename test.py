5b747f7ecd7ee70f7ec97277162ec8a71d0466b1
Igor Zhivilo <igorzhivilo@Igors-MacBook-Pro.local>
2020-05-11 01:07:07 +0300

    UT fails

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class TestHello(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_hello(self):
-        rv = self.app.get('/')
+        rv = self.app.get('/123')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
