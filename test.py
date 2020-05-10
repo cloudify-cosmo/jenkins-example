7c8d4d4806ad9088a957c7c6fe9b6c99f83d4ad4
Igor Zhivilo <igorzhivilo@Igors-MacBook-Pro.local>
2020-05-10 18:54:31 +0300

    Fix UT

--- test.py
+++ test.py
@@ -9,18 +9,18 @@ class TestHello(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_hello(self):
-        rv = self.app.get('/33')
+        rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_hello(self):
-        rv = self.app.get('/hello33/')
+        rv = self.app.get('/hello/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_name(self):
         name = 'Simon'
-        rv = self.app.get(f'/hello33/{name}')
+        rv = self.app.get(f'/hello/{name}')
         self.assertEqual(rv.status, '200 OK')
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
